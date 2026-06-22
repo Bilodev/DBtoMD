f14160a08fedf2bae5fc6467bcd70a03847e39b5
Bilodev <bilotta.antonio.biz@gmail.com>
2026-06-22 11:20:51 +0200

    refactoring

--- src/dbtomd/cli.py
+++ src/dbtomd/cli.py
@@ -1,32 +1,19 @@
 #!/bin/
 
-import mysql.connector
 import sys
 import os
 
+DB_USR = os.environ["DB_USR"]
+DB_PSW = os.environ["DB_PSW"]
 
-def main():
+
+def _mysql(db_name: str, out, MODE: int):
+    import mysql.connector
     db = mysql.connector.connect(
         host="localhost",
-        user=os.environ["SQLUSER"],
-        password=os.environ["SQLPSW"]
+        user=DB_USR,
+        password=DB_PSW
     )
-
-    if len(sys.argv) < 2:
-        exit("Error: DB name not specified")
-    if len(sys.argv) < 3:
-        exit("Error: output filename not specified")
-
-    db_name = sys.argv[1]
-    file_name = sys.argv[2]
-    slices = file_name.split(".")
-    if (slices[-1] != 'md'):
-        file_name = file_name + ".md"
-    MODE = 1
-    if len(sys.argv) == 4:
-        MODE = int(sys.argv[3])
-
-    out = open(file_name, 'w+')
 
     cursor = db.cursor()
 
@@ -35,10 +22,21 @@ def main():
 
     tables = [table_name for (table_name, ) in cursor]
 
-    if MODE == 0:
-        for t in tables:
-            out.write(f"# {t}\n")
-            cursor.execute(f"DESCRIBE {t}")
+    for t in tables:
+        out.write(f"# {t}\n")
+        cursor.execute(f"DESCRIBE {t}")
+        if MODE == 1:
+            out.write("| name | type | null | key | default | extra |\n")
+            out.write("|------|------|------|-----|---------|-------|\n")
+
+            for res in cursor:
+                name, type, null, key, default, extra = res
+                k = key if key else "/"
+                ex = extra if extra else "/"
+
+                out.write(
+                    f"| {name} | {type} | {null} | {k} | {default} | {ex} |\n")
+        elif MODE == 0:
             for res in cursor:
                 name, type, null, key, default, extra = res
                 out.write(f"##### {name}:\n")
@@ -50,17 +48,34 @@ def main():
                 ex = extra if extra else "/"
                 out.write(f"- **extra**: {ex}\n")
 
-    if MODE == 1:
-        for t in tables:
-            out.write(f"# {t}\n")
-            cursor.execute(f"DESCRIBE {t}")
-            out.write("| name | type | null | key | default | extra |\n")
-            out.write("|------|------|------|-----|---------|-------|\n")
 
-            for res in cursor:
-                name, type, null, key, default, extra = res
-                k = key if key else "/"
-                ex = extra if extra else "/"
+databases = {
+    "mysql": _mysql
+}
 
-                out.write(
-                    f"| {name} | {type} | {null} | {k} | {default} | {ex} |\n")
+
+def main():
+
+    if len(sys.argv) < 2:
+        exit("Error: DB type not specified")
+    if len(sys.argv) < 3:
+        exit("Error: DB name not specified")
+    if len(sys.argv) < 4:
+        exit("Error: output filename not specified")
+
+    db_type = sys.argv[1]
+    db_name = sys.argv[2]
+    file_name = sys.argv[3]
+    slices = file_name.split(".")
+    if (slices[-1] != 'md'):
+        file_name = file_name + ".md"
+    MODE = 1
+    if len(sys.argv) == 5:
+        MODE = int(sys.argv[4])
+
+    func = databases.get(db_type)
+    if not func:
+        exit(f"Error: DB type '{db_type}' not valid")
+    
+    out = open(file_name, 'w+')
+    func(db_name, out, MODE)
